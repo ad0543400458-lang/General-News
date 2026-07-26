0ab5a22d57cc04f1d8ccf2c5a411ab96667ea7d2
ad0543400458-lang <ad0543400458@gmail.com>
2026-07-26 18:31:03 +0300

    Update create_menu.py

--- create_menu.py
+++ create_menu.py
@@ -1,1 +1,17 @@
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import threading
 
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
+
+def start_server():
+    port = int(os.environ.get("PORT", 8080))
+    server = HTTPServer(('0.0.0.0', port), SimpleHandler)
+    server.serve_forever()
+
+# הפעלת השרת ברקע כדי לשמור על Render פעיל
+threading.Thread(target=start_server, daemon=True).start()
